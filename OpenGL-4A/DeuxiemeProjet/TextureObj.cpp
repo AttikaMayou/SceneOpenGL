8401e82874e80aaead7bc00df54f0f3ef945445c
Attika <attika.mayou@gmail.com>
2020-02-10 21:53:13 +0100

    Delete useless header and source files

--- OpenGL-4A/DeuxiemeProjet/TextureObj.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-#include "TextureObj.h"
-
-/*unsigned char* TextureObj::LoadText(int width, int height, int nbChannels) {
-	return data;
-}*/
--- OpenGL-4A/DeuxiemeProjet/TextureObj.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct TextureObj {
-private :
-
-public : 
-	//unsigned char* LoadText(int width, int height, int nbChannels);
-};
